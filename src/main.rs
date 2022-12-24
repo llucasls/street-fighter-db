f30f98b220180176d7c2e21883f371c5aefc3d78
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-12-24 19:01:26 -0300

    feat: implement function new

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ struct Fighter {
     nationality: String,
 }
 
-fn create_fighter(name: &str, style: &str, nationality: &str) -> Fighter {
-    return Fighter {
-        name: name.to_string(),
-        style: style.to_string(),
-        nationality: nationality.to_string(),
-    };
+impl Fighter {
+    fn new(name: &str, style: &str, nationality: &str) -> Fighter {
+        return Fighter {
+            name: name.to_string(),
+            style: style.to_string(),
+            nationality: nationality.to_string(),
+        };
+    }
 }
 
 fn main() {
-    let ryu = create_fighter("Ryu", "Karate", "Japanese");
+    let ryu = Fighter::new("Ryu", "Karate", "Japanese");
 
     println!("The chosen fighter is: {:?}", ryu);
 }
