9dcb3920163d591de43a4218069ace92000da43a
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-12-23 21:12:06 -0300

    feat: create Fighter struct

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
+#[derive(Debug)]
+struct Fighter {
+    name: String,
+    style: String,
+    nationality: String,
+}
+
+fn create_fighter(name: &str, style: &str, nationality: &str) -> Fighter {
+    return Fighter {
+        name: name.to_string(),
+        style: style.to_string(),
+        nationality: nationality.to_string(),
+    };
+}
+
 fn main() {
+    let ryu = create_fighter("Ryu", "Karate", "Japanese");
+
+    println!("The chosen fighter is: {:?}", ryu);
 }
